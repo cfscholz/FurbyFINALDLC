ca5cc99828bee0b7e06df45184665a7b791b1a87
Constantin Scholz <102663526+cfscholz@users.noreply.github.com>
2022-05-05 00:22:59 +0200

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -1,24 +1,31 @@
-
 [
+    {
+        "file": "context.dlc",
+        "title": "Context Logo",
+        "buttons": [
+            { "title": "Logo", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "hacked.dlc",
+        "title": "HACKED",
+        "buttons": [
+            { "title": "Hacked 1", "action": [75,0,3,4] },
+            { "title": "Hacked 2", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "noimg2.dlc",
+        "title": "Audio only",
+        "buttons": [
+            { "title": "Audio test ", "action": [75,0,4,4] }
+        ]
+    },
     {
         "file": "drum.dlc",
         "title": "DRUM",
         "buttons": [
             { "title": "DRUM ", "action": [75,0,2,4] }
-        ]
-    },
-    {
-        "file": "guitar.dlc",
-        "title": "GUITAR",
-        "buttons": [
-            { "title": "GUITAR ", "action": [75,0,3,4] }
-        ]
-    },
-
-        "file": "ursula.dlc",
-        "title": "URSULA",
-        "buttons": [
-            { "title": "URSULA ", "action": [75,0,4,4] }
         ]
     }
 ]
