112174b26fcbbcb3872d0697c51b3bcde24b9790
Constantin Scholz <102663526+cfscholz@users.noreply.github.com>
2022-05-05 00:39:18 +0200

    updated dlc upload json

--- dlc/index.js
+++ dlc/index.js
@@ -22,10 +22,24 @@
         ]
     },
     {
-        "file": "ursula.dlc",
-        "title": "ursula",
+        "file": "drum.dlc",
+        "title": "DRUM",
         "buttons": [
-            { "title": "Ursula ", "action": [75,0,3,4] }
+            { "title": "DRUM ", "action": [75,0,2,4] }
+        ]
+    },
+    {
+        "file": "ursula.dlc",
+        "title": "URSULA",
+        "buttons": [
+            { "title": "URSULA", "action": [75,0,3,4] }
+        ]
+    },
+    {
+        "file": "guitar.dlc",
+        "title": "GUITAR",
+        "buttons": [
+            { "title": "GUITAR", "action": [75,0,4,4] }
         ]
     }
 ]
