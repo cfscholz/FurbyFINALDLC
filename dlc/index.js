7608cbe8ae99b38b5223a9f7b0a027cf1801382f
Constantin Scholz <102663526+cfscholz@users.noreply.github.com>
2022-05-05 00:14:26 +0200

    added names

--- dlc/index.js
+++ dlc/index.js
@@ -1,23 +1,24 @@
+
 [
     {
         "file": "drum.dlc",
         "title": "DRUM",
         "buttons": [
-            { "title": "DRUMLO ", "action": [75,0,2,4] }
+            { "title": "DRUM ", "action": [75,0,2,4] }
         ]
     },
     {
         "file": "guitar.dlc",
         "title": "GUITAR",
         "buttons": [
-            { "title": "DRUMLO ", "action": [75,0,3,4] }
+            { "title": "GUITAR ", "action": [75,0,3,4] }
         ]
     },
 
         "file": "ursula.dlc",
         "title": "URSULA",
         "buttons": [
-            { "title": "DRUMLO ", "action": [75,0,4,4] }
+            { "title": "URSULA ", "action": [75,0,4,4] }
         ]
     }
 ]
